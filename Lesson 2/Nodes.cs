16d7ba15d9c5a2e9d6b356ea6926f96c6008e81e
akolygo <akolygo@gmail.com>
2021-07-02 12:57:30 +0300

    Lesson 2

---
+++ Lesson 2/Nodes.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2_algorhitms
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+    }
+
+    //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
+    public interface ILinkedList
+    {
+        int GetCount(); // возвращает количество элементов в списке
+        void AddNode(int value);  // добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанный элемент
+        Node FindNode(int searchValue); // ищет элемент по его значению
+    }
+
+    public class NodeItem : ILinkedList, IEnumerable
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        private int count;
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public void AddNode(int value)
+        {
+            var node = new Node() { Value = value };
+
+            if (Head == null)
+                Head = node;
+            else
+            {
+                Tail.NextNode = node;
+                node.PrevNode = Tail;
+            }
+            Tail = node;
+            count++;
+
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+
+            var findNode = FindNode(value);
+            if (findNode == null)
+            {
+                Console.WriteLine("No such element in the list.Your node HAS NOT been added");
+                return;
+            }
+            var nextItem = findNode.NextNode;
+            findNode.NextNode = node;
+            node.NextNode = nextItem;
+            count++;
+        }
+
+        public void RemoveNode(int index)
+        {
+            if (index == 0)
+            {
+                var newStartNode = Head.NextNode;
+                Head.NextNode = null;
+
+            }
+
+            int currentIndex = 0;
+            var currentNode = Head;
+            while (currentNode != null)
+            {
+                if (currentIndex == index - 1)
+                {
+                    RemoveNode(currentNode);
+
+                }
+
+                currentNode = currentNode.NextNode;
+                currentIndex++;
+            }
+
+        }
+
+        public void RemoveNode(Node node)
+        {
+            var searchNode = FindNode(node.Value);
+            if (searchNode == Head)
+            {
+                Head = Head.NextNode;
+                count--;
+                return;
+            }
+            if (searchNode.NextNode == null)
+            {
+                Tail = searchNode.PrevNode;
+                count--;
+                return;
+            }
+            var nextItem = searchNode.NextNode;
+            var prevItem = searchNode.PrevNode;
+            prevItem.NextNode = nextItem;
+            count--;
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            Node result = Head;
+            while (result != null)
+            {
+                if (result.Value == searchValue)
+                    return result;
+                result = result.NextNode;
+            }
+            return null;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.NextNode;
+            }
+        }
+
+    }
+
+}
---
+++ Lesson 2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Lesson_2_algorhitms
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var root = new NodeItem();
+
+            InitializeNode(root, 100);
+            PrintThisShit(root);
+            FuncTest(root);
+            Console.WriteLine("===");
+            PrintThisShit(root);
+        }
+
+        static void FuncTest(NodeItem root)
+        {
+            root.AddNode(110);
+            var nodeAdd = new Node() { Value = 101 };
+            root.AddNodeAfter(nodeAdd, 2);
+            var nodeRem = new Node() { Value = 8 };
+            root.RemoveNode(nodeRem);
+            root.RemoveNode(7);
+        }
+        static void InitializeNode(NodeItem root, int i)
+        {
+            
+            for (i = 1; i <= 10; i++) root.AddNode(i);
+            
+        }
+
+        static void PrintThisShit(NodeItem root)
+        {
+            Console.WriteLine($"This node has {root.GetCount()} elements");
+            foreach (var elem in root)
+            {
+                Console.WriteLine(elem);
+            }
+
+            
+        }
+    }
+}
